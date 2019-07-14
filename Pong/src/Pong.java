696300398a1cbcf5413bdc6cb3137d0391cff453
AmyS-OP <AmySutich@myop.ac.nz>
2019-07-14 18:54:24 +1200

    Push to GIT

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 public class Pong extends JFrame {
 	
-	private final static String WINDOW_TITLE = "Pong";
+	private final static String WINDOW_TITLE = "Pong"; 
 	private final static int WINDOW_WIDTH = 800;
 	private final static int WINDOW_HEIGHT = 600;
 	
